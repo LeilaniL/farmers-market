aee7c003b074803b9097cee453e52bdb88d9497b
Leilani Leach <leilanileach@yahoo.com>
2019-02-13 12:53:02 -0800

    add styles

--- src/components/TodayInfo.jsx
+++ src/components/TodayInfo.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
-function TodayInfo() {
+function TodayInfo(props) {
+  var todayStyles = {
+    backgroundColor: '#ffd3a3',
+    borderRadius: '4px',
+    width: '400px',
+    height: '600px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: '25px'
+  }
   return (
-    <div>
+    <div style={todayStyles}>
       <h3>Today:</h3>
     </div>
   )
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import TodayInfo from './TodayInfo';
 
-function Welcome() {
+function Welcome(props) {
+  var welcomeStyles = {
+    height: '100%',
+    backgroundColor: '#a3ffa8',
+    fontFamily: 'sans-serif',
+    textAlign: 'center',
+    paddingTop: '50px'
+  }
   return (
-    <div>
+    <div style={welcomeStyles}>
       <h1>Welcome to Avery's Organics</h1>
       <TodayInfo />
       {/* <WeeklySchedule /> */}
       {/* <MonthlyProduceList /> */}
-    </div>
+    </div >
   )
 }
 export default Welcome;
