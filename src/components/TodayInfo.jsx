50f1f09fd2bf33ddf8e7881cb0c47219cee11dc1
Leilani Leach <leilanileach@yahoo.com>
2019-02-13 17:21:02 -0800

    add current date info

--- src/components/TodayInfo.jsx
+++ src/components/TodayInfo.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import WeeklySchedule from './WeeklySchedule';
 
 function TodayInfo(props) {
+  var currentDay = new Date();
+  WeeklySchedule
+
   var todayStyles = {
     backgroundColor: '#ffd3a3',
     borderRadius: '4px',
     width: '400px',
-    height: '600px',
+    height: '400px',
     marginLeft: 'auto',
     marginRight: 'auto',
     padding: '25px'
   }
   return (
     <div style={todayStyles}>
-      <h3>Today:</h3>
+
+      <h3>Today: {currentDay.toLocaleDateString()}</h3>
       <p><strong>You can find us at:</strong></p>
       <ul>
         <li>Location</li>
